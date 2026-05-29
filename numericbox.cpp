55ab73239de26182bc3638230c493cead0865340
Suhas-Khatokar <suhas.khatokar13@gmail.com>
2026-05-29 18:03:54 +0530

    Numerical box pattern

---
+++ numericbox.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter no of rows/columns: ";
+    cin>>n;
+
+    for (int i=1; i<=n; i++) {
+        for (int j=i; j<=n; j++) {
+            cout << j;
+        }
+        for (int k=1; k<i; k++) {
+            cout << k;
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}
