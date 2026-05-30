7872530d5378349b35e39a84de952a99a62d859c
Suhas-Khatokar <suhas.khatokar13@gmail.com>
2026-05-30 11:42:59 +0530

    Reverse number program

---
+++ reversenum.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include <cmath>
+
+using namespace std;
+
+int main () {   
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    int revnum = 0;
+
+    while (n>0) {
+        int rem = n%10;
+        revnum = revnum*10 + rem;
+        n=n/10;
+    }
+
+    cout<<"Reversed number: "<<revnum;
+
+    return 0;
+}
