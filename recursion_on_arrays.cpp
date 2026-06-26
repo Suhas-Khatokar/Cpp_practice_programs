603c96f1d1f64858fc8f195dc1e7c7e9a1336ed6
Suhas-Khatokar <suhas.khatokar13@gmail.com>
2026-06-26 11:34:31 +0530

    Array operations using recursion

--- recursion_on_arrays.cpp
+++ recursion_on_arrays.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 void printArray(int *arr, int n, int i) {
@@ -17,9 +18,32 @@ int sumOfElements (int *arr, int n, int i) {
     return arr[i] + sumOfElements(arr, n, i-1);
 }
 
+void findElement(int *arr, int n, int i, int x) {
+    if (i==n){
+        cout<<"No";
+        return; 
+    } 
+    if (x==arr[i]){
+        cout<<"Yes";
+        return;
+    }
+
+    findElement(arr, n, i+1, x);
+}
+
+void subsetSum(int *arr, int n, int i, vector <int> &result, int sum) {
+    if (i==n) {
+        result.push_back(sum);
+        return;
+    }
+
+    subsetSum(arr, n, i+1, result, sum + arr[i]); //pick the ith element
+    subsetSum(arr, n, i+1, result, sum); //do not pick the ith element
+}
+
 int main() {
 
-    int arr[6] = {2, 3, 5, 20, 1, 0};
+    /*int arr[6] = {2, 3, 5, 20, 1, 0};
     printArray(arr, 6, 0);
 
     int max_ele = maxElement(arr, 6, 0);
@@ -27,6 +51,16 @@ int main() {
     
     int sum = sumOfElements(arr, 6, 5);
     cout<<sum;
+
+    findElement(arr, 6, 1, 45);*/
+
+    int arr[] = {1, 2, 3};
+    vector<int> result;
+    subsetSum(arr, 3, 0, result, 0);
+
+    for (int i=0; i<result.size(); i++) {
+        cout<<result[i]<<" ";
+    }
     
     return 0;
 }
